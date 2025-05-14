49bd2162a6409aca4d619e6ef713889bf06d8f25
antoninhenriet <115776565+antoninhenriet@users.noreply.github.com>
2025-05-14 23:48:21 +0200

    Décompte des modalités

---
+++ Modalités.R
@@ -0,0 +1,32 @@
+### Ce script permet de réaliser les statistiques descriptives d'ensemble donnant ###
+### le profil global des répondants dans la base ###
+
+#Lecture des packages
+library(haven)
+library(tidyverse)
+
+#Lecture de la base
+base <- read_dta("LYCEES_base.dta")
+
+#Sélection des variables
+var <- base %>%
+  select(genre,
+         clust)
+
+#Décompte des modalités
+resultats <- lapply(var, function(x) {
+  effectif <- table(x)
+  frequence <- prop.table(effectif)
+  
+  data.frame(
+    Modalité = names(effectif),
+    Effectif = as.vector(effectif),
+    Fréquence = round(as.vector(frequence), 3)
+  )
+})
+
+#Affichage des modalités
+for(nom_var in names(resultats)) {
+  cat("\nVariable :", nom_var, "\n")
+  print(resultats[[nom_var]])
+}
